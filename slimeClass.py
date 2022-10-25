82dbde8b7fbeefc1cef7f2f6c1a4755fa451998e
NoahTheCorgi <91349811+NoahTheCorgi@users.noreply.github.com>
2022-10-25 04:52:48 -0400

    format

--- slimeClass.py
+++ slimeClass.py
@@ -1,41 +1,42 @@
 import os
 import pygame
 
+
 class slime:
 
-	empCount = 0
-	slimesList = []
-	#location is tuple and x and y are velocity
-	def __init__(self, defaultImage, name, location, x, y):
-		self.veloctiy = (x,y)
+    empCount = 0
+    slimesList = []
+    # location is tuple and x and y are velocity
+    def __init__(self, defaultImage, name, location, x, y):
+        self.veloctiy = (x, y)
 
-		if type(name) is str:
-			self.name = name
-		else:
-			print("first attribute name must be a str")
-		if type(location) is tuple:
-			self.location = location
-		else:
-			print("second attribute color must be a str")
-		slime.empCount += 1
-		slime.slimesList.append(self)
-		
-		#list of string elements that refer to the image file
-		self.animation = [defaultImage]
-		self.slimeSurface = pygame.image.load(self.animation[0])
-		self.rectangle = self.slimeSurface.get_rect(center = location)
+        if type(name) is str:
+            self.name = name
+        else:
+            print("first attribute name must be a str")
+        if type(location) is tuple:
+            self.location = location
+        else:
+            print("second attribute color must be a str")
+        slime.empCount += 1
+        slime.slimesList.append(self)
 
-	def displayCount(self):
-		print ("Total number of slimes %d" % slime.empCount)
+        # list of string elements that refer to the image file
+        self.animation = [defaultImage]
+        self.slimeSurface = pygame.image.load(self.animation[0])
+        self.rectangle = self.slimeSurface.get_rect(center=location)
 
-	def displaySlime(self):
-		print ("Name : ", self.name,  ", Color: ", self.color)
+    def displayCount(self):
+        print("Total number of slimes %d" % slime.empCount)
 
-	def reset_animation(self, string):
-		self.animation = []
+    def displaySlime(self):
+        print("Name : ", self.name, ", Color: ", self.color)
 
-	def set_animation(self, folder):
-		 # folder is the string of directory folder of animations (with the correctly ordered names)
-		if type(folder) is str:
-			for item in sorted(os.listdir("./" + folder)):
-				self.animation.append("animation/" + item)
+    def reset_animation(self, string):
+        self.animation = []
+
+    def set_animation(self, folder):
+        # folder is the string of directory folder of animations (with the correctly ordered names)
+        if type(folder) is str:
+            for item in sorted(os.listdir("./" + folder)):
+                self.animation.append("animation/" + item)
--- slimes.py
+++ slimes.py
@@ -2,9 +2,11 @@
 
 import os
 import random
-os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
+
+os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "hide"
 import pygame, sys, slimeClass, time
-#from pygame.locals import *
+
+# from pygame.locals import *
 
 # set up for general variables and general prepartion
 pygame.init()
@@ -16,8 +18,8 @@ slimesArray = []
 
 black = 0, 0, 0
 red = 200, 0, 0
-green = 0,200,0
-white = 255,255,255
+green = 0, 200, 0
+white = 255, 255, 255
 
 size = width, height = 800, 600
 screen = pygame.display.set_mode(size)
@@ -30,9 +32,9 @@ x = 200
 y = 200
 ##############################################
 
-#this is how you update the slimeTheSlime slime location
+# this is how you update the slimeTheSlime slime location
 PlayerSlime = pygame.image.load("animation/green_slime_0.png")
-PlayerSlime_rect = PlayerSlime.get_rect(center = (x,y))
+PlayerSlime_rect = PlayerSlime.get_rect(center=(x, y))
 playerLifePoints = 100
 playerCombatScore = 0
 playerTime = 0
@@ -43,20 +45,23 @@ gameOver = False
 ###############___set up for displaying the title etc###############
 ####################################################################
 pygame.font.init()
-myfont = pygame.font.SysFont('Comic Sans MS', 28)
-textsurface = myfont.render('Slimes Game - NoahTheCorgi -- Press Space to Speed Up! :D', False, (0, 200, 0))
+myfont = pygame.font.SysFont("Comic Sans MS", 28)
+textsurface = myfont.render(
+    "Slimes Game - NoahTheCorgi -- Press Space to Speed Up! :D", False, (0, 200, 0)
+)
 ####################################################################
 ####################################################################
 
 
 ########___predefined functions to use later___#######
-def check_inside_screen(x,y):
-    if (0<x<width) and (0<y<height):
+def check_inside_screen(x, y):
+    if (0 < x < width) and (0 < y < height):
         return True
     else:
         return False
 
-def keep_inside_screen(x,y):
+
+def keep_inside_screen(x, y):
     if x < 0:
         x = 1
     if x > width:
@@ -65,6 +70,7 @@ def keep_inside_screen(x,y):
         y = 1
     if y > width:
         y = height - 1
+
 
 def checkIfCollision():
     for i in range(len(slimesArray)):
@@ -80,8 +86,12 @@ def checkIfCollision():
 """Set up for the first original non slimeTheSlime slime (this should thus be integrated for the rest of the slimes"""
 for i in range(20):
     slimeImage = pygame.image.load("animation/red_slime.png")
-    slimeRectangle = slimeImage.get_rect(center = (random.randint(0, 500), random.randint(0, 500)))
-    slimesArray.append([slimeImage, slimeRectangle, [random.randint(-5, 5), random.randint(-5, 5)]])
+    slimeRectangle = slimeImage.get_rect(
+        center=(random.randint(0, 500), random.randint(0, 500))
+    )
+    slimesArray.append(
+        [slimeImage, slimeRectangle, [random.randint(-5, 5), random.randint(-5, 5)]]
+    )
 
 
 ########################################################
@@ -89,7 +99,7 @@ for i in range(20):
 ########################################################
 counter = 0
 while True:
-    
+
     # optional::
     # time.sleep(1)
 
@@ -98,8 +108,16 @@ while True:
             playerLifePoints += 3
             for i in range(3):
                 slimeImage = pygame.image.load("animation/red_slime.png")
-                slimeRectangle = slimeImage.get_rect(center = (random.randint(0, 500), random.randint(0, 500)))
-                slimesArray.append([slimeImage, slimeRectangle, [random.randint(-5, 5), random.randint(-5, 5)]])
+                slimeRectangle = slimeImage.get_rect(
+                    center=(random.randint(0, 500), random.randint(0, 500))
+                )
+                slimesArray.append(
+                    [
+                        slimeImage,
+                        slimeRectangle,
+                        [random.randint(-5, 5), random.randint(-5, 5)],
+                    ]
+                )
         counter = 0
 
     ###################___Get information about user inputs___##################
@@ -143,139 +161,137 @@ while True:
             elif event.key == pygame.K_SPACE or event.key == pygame.K_RSHIFT:
                 keys[4] = False
 
-
     if playerLifePoints > 0:
 
         ########################___Update with respect to the user inputs#########################
 
         if keys[4]:
-            #for i in range(len(slimesArray)):
+            # for i in range(len(slimesArray)):
             #    slimerect = slimesArray[i][1]
             #    valid = False
-            if counter%25 < 13:
+            if counter % 25 < 13:
                 print(0)
                 PlayerSlime = pygame.image.load("animation/green_slime_0.png")
             else:
                 print(1)
                 PlayerSlime = pygame.image.load("animation/green_slime_1.png")
-            if keys[0]:#UP
-                keep_inside_screen(x,y)
-                #if check_inside_screen(x, y-10) and not slimerect.colliderect(PlayerSlime_rect):
+            if keys[0]:  # UP
+                keep_inside_screen(x, y)
+                # if check_inside_screen(x, y-10) and not slimerect.colliderect(PlayerSlime_rect):
                 if check_inside_screen(x, y - 10) and not checkIfCollision():
-                    y-=10
+                    y -= 10
                     PlayerSlime_rect.centery -= 10
                 else:
                     playerLifePoints -= random.randint(1, 10)
                     playerCombatScore += 1
-            if keys[1]:#LEFT
-                keep_inside_screen(x,y)
-                if check_inside_screen(x-10, y) and not checkIfCollision():
-                    x-=10
+            if keys[1]:  # LEFT
+                keep_inside_screen(x, y)
+                if check_inside_screen(x - 10, y) and not checkIfCollision():
+                    x -= 10
                     PlayerSlime_rect.centerx -= 10
                 else:
                     playerLifePoints -= random.randint(1, 10)
                     playerCombatScore += 1
-            if keys[2]:#DOWN
-                keep_inside_screen(x,y)
-                if check_inside_screen(x, y+10) and not checkIfCollision():
-                    y+=10
+            if keys[2]:  # DOWN
+                keep_inside_screen(x, y)
+                if check_inside_screen(x, y + 10) and not checkIfCollision():
+                    y += 10
                     PlayerSlime_rect.centery += 10
                 else:
                     playerLifePoints -= random.randint(1, 10)
                     playerCombatScore += 1
-            if keys[3]:#RIGHT
-                keep_inside_screen(x+10,y)
-                if check_inside_screen(x+10, y) and not checkIfCollision():
-                    x+=10
+            if keys[3]:  # RIGHT
+                keep_inside_screen(x + 10, y)
+                if check_inside_screen(x + 10, y) and not checkIfCollision():
+                    x += 10
                     PlayerSlime_rect.centerx += 10
                 else:
                     playerLifePoints -= random.randint(1, 10)
                     playerCombatScore += 1
         else:
-            if counter%25 < 13:
+            if counter % 25 < 13:
                 PlayerSlime = pygame.image.load("animation/slime.png")
             else:
                 PlayerSlime = pygame.image.load("animation/slime_1.png")
             if checkIfCollision():
                 playerLifePoints -= random.randint(1, 10)
                 playerCombatScore += 1
-            if keys[0]:#UPs
-                keep_inside_screen(x,y)
-                #if check_inside_screen(x, y-10) and not slimerect.colliderect(PlayerSlime_rect):
+            if keys[0]:  # UPs
+                keep_inside_screen(x, y)
+                # if check_inside_screen(x, y-10) and not slimerect.colliderect(PlayerSlime_rect):
                 if check_inside_screen(x, y - 10) and not checkIfCollision():
-                    y-=5
+                    y -= 5
                     PlayerSlime_rect.centery -= 5
                 else:
                     playerLifePoints -= random.randint(1, 10)
                     playerCombatScore += 1
-            if keys[1]:#LEFT
-                keep_inside_screen(x,y)
-                if check_inside_screen(x-10, y) and not checkIfCollision():
+            if keys[1]:  # LEFT
+                keep_inside_screen(x, y)
+                if check_inside_screen(x - 10, y) and not checkIfCollision():
                     x -= 5
                     PlayerSlime_rect.centerx -= 5
                 else:
                     playerLifePoints -= random.randint(1, 10)
                     playerCombatScore += 1
-            if keys[2]:#DOWN
-                keep_inside_screen(x,y)
-                if check_inside_screen(x, y+10) and not checkIfCollision():
+            if keys[2]:  # DOWN
+                keep_inside_screen(x, y)
+                if check_inside_screen(x, y + 10) and not checkIfCollision():
                     y += 5
                     PlayerSlime_rect.centery += 5
                 else:
                     playerLifePoints -= random.randint(1, 10)
                     playerCombatScore += 1
-            if keys[3]:#RIGHT
-                keep_inside_screen(x+10,y)
-                if check_inside_screen(x+10, y) and not checkIfCollision():
-                    x+=5
+            if keys[3]:  # RIGHT
+                keep_inside_screen(x + 10, y)
+                if check_inside_screen(x + 10, y) and not checkIfCollision():
+                    x += 5
                     PlayerSlime_rect.centerx += 5
                 else:
                     playerLifePoints -= random.randint(1, 10)
                     playerCombatScore += 1
         ###################################################################
 
-
-
         ##################___UPDATE ALL WITH RESPECT TO VELOCITY... ALL SPRITES___#######################
         #################################################################################################
         #################################################################################################
         for i in range(len(slimesArray)):
-            #print("was here ")
+            # print("was here ")
             slimerect = slimesArray[i][1]
-            #print(slimerect)
+            # print(slimerect)
             slimesArray[i][1] = slimesArray[i][1].move(slimesArray[i][2])
             if slimesArray[i][1].left < 0:
                 slimesArray[i][1].left = 1
-                slimesArray[i][2][0] = -slimesArray[i][2][0] * random.randint(1,2)
+                slimesArray[i][2][0] = -slimesArray[i][2][0] * random.randint(1, 2)
             if slimesArray[i][1].right > width:
-                slimesArray[i][1].right = width -1
-                slimesArray[i][2][0] = -slimesArray[i][2][0] * random.randint(1,2)
+                slimesArray[i][1].right = width - 1
+                slimesArray[i][2][0] = -slimesArray[i][2][0] * random.randint(1, 2)
 
-            if slimesArray[i][1].top < 0 :
+            if slimesArray[i][1].top < 0:
                 slimesArray[i][1].top = 1
-                slimesArray[i][2][1] = -slimesArray[i][2][1] * random.randint(1,2)
+                slimesArray[i][2][1] = -slimesArray[i][2][1] * random.randint(1, 2)
             if slimesArray[i][1].bottom > height:
-                slimesArray[i][1].bottom = height -1
-                slimesArray[i][2][1] = -slimesArray[i][2][1] * random.randint(1,2)
+                slimesArray[i][1].bottom = height - 1
+                slimesArray[i][2][1] = -slimesArray[i][2][1] * random.randint(1, 2)
 
-            x_axis_diff = (slimesArray[i][1].center[0] - PlayerSlime_rect.center[0])
-            y_axis_diff = (slimesArray[i][1].center[1] - PlayerSlime_rect.center[1])
+            x_axis_diff = slimesArray[i][1].center[0] - PlayerSlime_rect.center[0]
+            y_axis_diff = slimesArray[i][1].center[1] - PlayerSlime_rect.center[1]
             if slimesArray[i][1].colliderect(PlayerSlime_rect):
-                if x_axis_diff!=0 and y_axis_diff!=0:
-                    slimesArray[i][2][0] = 2*x_axis_diff/abs(x_axis_diff)
-                    slimesArray[i][2][1] = 2*y_axis_diff/abs(y_axis_diff)
-        
+                if x_axis_diff != 0 and y_axis_diff != 0:
+                    slimesArray[i][2][0] = 2 * x_axis_diff / abs(x_axis_diff)
+                    slimesArray[i][2][1] = 2 * y_axis_diff / abs(y_axis_diff)
+
         helperIndex = 0
         while helperIndex < len(slimesArray):
-            if (slimesArray[helperIndex][2][0] >= 100 or slimesArray[helperIndex][2][1] >= 100):
+            if (
+                slimesArray[helperIndex][2][0] >= 100
+                or slimesArray[helperIndex][2][1] >= 100
+            ):
                 slimesArray.pop(helperIndex)
                 helperIndex -= 1
             helperIndex += 1
         ###################################################################
         ###################################################################
         ###################################################################
-
-
 
         #########################_ANIMATE_#################################
         ###################################################################
@@ -296,19 +312,40 @@ while True:
         ###################################################################
         ###################################################################
 
-
         #########################___Pygame_blits___#############################
         ########################################################################
         screen.fill(white)
-        screen.blit(textsurface, (0,0))
-        screen.blit(myfont.render("LifePointsLeft: " + str(playerLifePoints), False, (200, 0, 0)), (10, 35))
-        screen.blit(myfont.render("Combat Score: " + str(playerCombatScore), False, (200, 100, 0)), (10, 65))
-        screen.blit(myfont.render("Time/Frames Survived: " + str(playerTime), False, (200, 200, 100)), (10, 95))
-        screen.blit(myfont.render("Be Careful! Hitting the boundaries reduces LifePointes!! ", False, (200, 0, 0)), (10, 550))
+        screen.blit(textsurface, (0, 0))
+        screen.blit(
+            myfont.render(
+                "LifePointsLeft: " + str(playerLifePoints), False, (200, 0, 0)
+            ),
+            (10, 35),
+        )
+        screen.blit(
+            myfont.render(
+                "Combat Score: " + str(playerCombatScore), False, (200, 100, 0)
+            ),
+            (10, 65),
+        )
+        screen.blit(
+            myfont.render(
+                "Time/Frames Survived: " + str(playerTime), False, (200, 200, 100)
+            ),
+            (10, 95),
+        )
+        screen.blit(
+            myfont.render(
+                "Be Careful! Hitting the boundaries reduces LifePointes!! ",
+                False,
+                (200, 0, 0),
+            ),
+            (10, 550),
+        )
         # player rendered first
         screen.blit(PlayerSlime, PlayerSlime_rect)
         # all the npc slimes rendered
-        for i in range (len(slimesArray)):
+        for i in range(len(slimesArray)):
             screen.blit(slimesArray[i][0], slimesArray[i][1])
         ######## test slime ####### <---- SWITCH TO UPDATE NON slimeTheSlime SPRITES
         # screen.blit(slimeTheSlime.slimeSurface, slimeTheSlime.rectangle)
@@ -330,10 +367,32 @@ while True:
         gameOver = True
         screen.fill(white)
         screen.blit(textsurface, (0, 0))
-        screen.blit(myfont.render("LifePointsLeft: " + str(playerLifePoints), False, (200, 0, 0)), (10, 35))
-        screen.blit(myfont.render("Combat Score: " + str(playerCombatScore), False, (200, 100, 0)), (10, 65))
-        screen.blit(myfont.render("Time/Frames Survived: " + str(playerTime), False, (200, 200, 100)), (10, 95))
-        screen.blit(myfont.render("Be Careful! Hitting the boundaries reduces LifePointes!! ", False, (200, 0, 0)), (10, 550))
+        screen.blit(
+            myfont.render(
+                "LifePointsLeft: " + str(playerLifePoints), False, (200, 0, 0)
+            ),
+            (10, 35),
+        )
+        screen.blit(
+            myfont.render(
+                "Combat Score: " + str(playerCombatScore), False, (200, 100, 0)
+            ),
+            (10, 65),
+        )
+        screen.blit(
+            myfont.render(
+                "Time/Frames Survived: " + str(playerTime), False, (200, 200, 100)
+            ),
+            (10, 95),
+        )
+        screen.blit(
+            myfont.render(
+                "Be Careful! Hitting the boundaries reduces LifePointes!! ",
+                False,
+                (200, 0, 0),
+            ),
+            (10, 550),
+        )
         ##################################
         # player rendered first
         screen.blit(PlayerSlime, PlayerSlime_rect)
@@ -341,7 +400,12 @@ while True:
         for i in range(len(slimesArray)):
             screen.blit(slimesArray[i][0], slimesArray[i][1])
 
-        screen.blit(myfont.render("Game Over at Time/Frame: " + str(playerTime), False, (200, 0, 0)), (300, 300))
+        screen.blit(
+            myfont.render(
+                "Game Over at Time/Frame: " + str(playerTime), False, (200, 0, 0)
+            ),
+            (300, 300),
+        )
 
         ########___UPDATE EVERYTHING THAT HAD BEEN PREPARED FOR THE NEXT FRAME___########
         pygame.time.wait(26)
@@ -350,7 +414,6 @@ while True:
         if keys[5]:
             pygame.quit()
             sys.exit()
-
 
     counter += 1
 
